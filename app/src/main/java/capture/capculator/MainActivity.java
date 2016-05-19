9c05d1a07cd4394e18f144e423d97d96de5dfc4a
selinawj <selinawj@msn.com>
2016-05-19 22:36:48 +0800

    Removed LoginActivity Added transitional Homepage Set color palette1

--- app/src/main/java/capture/capculator/MainActivity.java
+++ app/src/main/java/capture/capculator/MainActivity.java
@@ -1,5 +1,6 @@
 package capture.capculator;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,9 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //display the logo during 2 seconds
+        new CountDownTimer(2000,1000){
+            @Override
+            public void onTick(long millisUntilFinished){}
+
+            @Override
+            public void onFinish(){
+                //set the new Content of your activity
+                MainActivity.this.setContentView(R.layout.activity_home);
+            }
+        }.start();
     }
-    public void buttonOnClick(View v){
-        Button button = (Button) v;
-        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-    }
+
+    /*this is to link buttons to another Activity*/
+    //public void buttonOnClick(View v){
+      //  Button button = (Button) v;
+        //startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+    //}
 }
