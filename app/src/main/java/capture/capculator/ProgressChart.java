7f7e0c2d73851f44ab2a2132859ffcdabbae765c
topginnie3 <topginnie3@hotmail.com>
2016-06-01 23:58:57 +0800

    homepage

---
+++ app/src/main/java/capture/capculator/ProgressChart.java
@@ -0,0 +1,13 @@
+package capture.capculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProgressChart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress_chart);
+    }
+}
