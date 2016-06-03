199234743b54092c5b07deaca832c81aab7e0260
topginnie3 <topginnie3@hotmail.com>
2016-06-03 22:23:31 +0800

    java link for addmod button (failed)

--- app/src/main/java/capture/capculator/Home.java
+++ app/src/main/java/capture/capculator/Home.java
@@ -1,7 +1,12 @@
 package capture.capculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        final ImageButton bAddModule = (ImageButton) findViewById(R.id.bAddModule);
+
+        bAddModule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addmoduleIntent = new Intent(Home.this, AddModule.class);
+                Home.this.startActivity(addmoduleIntent);
+
+            }
+        });
     }
 }
