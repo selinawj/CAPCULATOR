6085dbcc4cc1b65969f4b33088fc43d25a0e4ef4
topginnie3 <topginnie3@hotmail.com>
2016-06-10 00:59:28 +0800

    1) Linked Add Module to CA Components. 2) Changed etTargetOverallMarks to Number(Decimal).

--- app/src/main/java/capture/capculator/AddModule.java
+++ app/src/main/java/capture/capculator/AddModule.java
@@ -1,7 +1,11 @@
 package capture.capculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class AddModule extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class AddModule extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_module);
+
+        final Button bAddSemester = (Button) findViewById(R.id.bAddSemester);
+
+        final Button bCAComponents = (Button) findViewById(R.id.bCAComponents);
+
+        bCAComponents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cacomponentsIntent = new Intent(AddModule.this, CAComponents.class);
+                AddModule.this.startActivity(cacomponentsIntent);
+
+            }
+        });
+
+
     }
 }
