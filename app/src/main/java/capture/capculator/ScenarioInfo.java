8a4469c6f03bea05df2200c10964d170814767fa
selinawj <selinawj@msn.com>
2016-06-15 02:01:54 +0800

    Edited ScenarioInfo layout

--- app/src/main/java/capture/capculator/ScenarioInfo.java
+++ app/src/main/java/capture/capculator/ScenarioInfo.java
@@ -2,12 +2,21 @@ package capture.capculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.NumberPicker;
 
 public class ScenarioInfo extends AppCompatActivity {
+
+    NumberPicker numPicker = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scenario_info);
+
+        numPicker = (NumberPicker)findViewById(R.id.etMCs);
+        numPicker.setMaxValue(9);
+        numPicker.setMinValue(0);
+        numPicker.setWrapSelectorWheel(false);
+
     }
 }
