6354cbc3b062330eb55685a35276e79a41fba856
topginnie3 <topginnie3@hotmail.com>
2016-06-14 13:20:42 +0800

    java link target setting -- ca components

--- app/src/main/java/capture/capculator/TargetSetting.java
+++ app/src/main/java/capture/capculator/TargetSetting.java
@@ -1,7 +1,13 @@
 package capture.capculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class TargetSetting extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class TargetSetting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_target_setting);
+
+        final EditText etTargetCAP = (EditText) findViewById(R.id.etTargetCAP);
+
+        final Button bFinalGradeTargetSetting = (Button) findViewById(R.id.bFinalGradeTargetSetting);
+
+        bFinalGradeTargetSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent CAComponentsIntent = new Intent(TargetSetting.this, CAComponents.class);
+                TargetSetting.this.startActivity(CAComponentsIntent);
+
+            }
+        });
+
     }
 }
