3b98798e6c28c9dc7890a09774f0d8604e8d272d
selinawj <selinawj@msn.com>
2016-06-15 23:37:44 +0800

    CurrentScenarios layout

--- app/src/main/java/capture/capculator/CurrentScenario.java
+++ app/src/main/java/capture/capculator/CurrentScenario.java
@@ -2,12 +2,18 @@ package capture.capculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.ToggleButton;
 
 public class CurrentScenario extends AppCompatActivity {
+
+    //ToggleButton scenario1 = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_scenario);
+
+        //cenario1 = (ToggleButton)findViewById(R.id.sc1);
     }
 }
