0216d0c6e435fadda98812fc2cda49e925a2e0dc
selinawj <selinawj@msn.com>
2016-06-18 02:53:31 +0800

    Added ExamReminderDetails

--- app/src/main/java/capture/capculator/ExamReminder.java
+++ app/src/main/java/capture/capculator/ExamReminder.java
@@ -1,7 +1,11 @@
 package capture.capculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ExamReminder extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class ExamReminder extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam_reminder);
+
+         /*this is to link buttons to ExamReminderDetails*/
+        final Button select = (Button) findViewById(R.id.select);
+
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent details = new Intent(ExamReminder.this, ExamReminderDetails.class);
+                ExamReminder.this.startActivity(details);
+
+            }
+        });
     }
 }
