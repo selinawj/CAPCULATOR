6ddaf323c33c8713c622067a40f39b687cc7788f
topginnie3 <topginnie3@hotmail.com>
2016-07-22 06:10:35 +0800

    conversion and calculator method added to home java class

--- app/src/main/java/capture/capculator/Home.java
+++ app/src/main/java/capture/capculator/Home.java
@@ -1,4 +1,6 @@
 package capture.capculator;
+final DBAdapter db = new DBAdapter(this);
+db.openDB();
 
 import android.content.Context;
 import android.content.Intent;
@@ -26,36 +28,201 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        final DBAdapter db=new DBAdapter(this);
+
+        }
 
         /*loads CAP and displays it*/
         TextView capView = (TextView) findViewById(R.id.capView);
-        db.openDB();
 
-        //RETRIEVE
-        Cursor c=db.getAllNames();
+        public double convertGrade(String grade) {
 
-        while(c.moveToNext()) {
-            String cap = c.getString(3);
+            //RETRIEVE
+            Cursor c = db.getAllNames();
 
+            double numCap = 0.00;
 
-            switch (cap) {
-                case "A":
-                    capView.setText("5.00");
-                    break;
-                case "B":
-                    capView.setText("4.00");
-                    break;
-                case "C":
-                    capView.setText("3.00");
-                    break;
-                case "D":
-                    capView.setText("2.00");
-                    break;
-                case "E":
-                    capView.setText("1.00");
-                    break;
+            while (c.moveToNext()) {
+                String cap = c.getString(3);
+
+                if (cap == "A+") {
+
+                    numCap = 5.00;
+
+                } else if (cap == "A") {
+
+                    numCap = 5.00;
+
+                } else if (cap == "A-") {
+
+                    numCap = 4.50;
+
+                } else if (cap == "B+") {
+
+                    numCap = 4.00;
+
+                } else if (cap == "B") {
+
+                    numCap = 3.50;
+
+                } else if (cap == "B-") {
+
+                    numCap = 3.00;
+
+                } else if (cap == "C+") {
+
+                    numCap = 2.50;
+
+                } else if (cap == "C") {
+
+                    numCap = 2.00;
+
+                } else if (cap == "D+") {
+
+                    numCap = 1.50;
+
+                } else if (cap == "D") {
+
+                    numCap = 1.00;
+
+                } else {
+
+                    numCap = 0.00;
+
+                }
+
             }
+            return numCap;
+
+        }
+
+
+/*
+                switch (cap) {
+                    case "A+":
+                        capView.setText("5.00");
+                        break;
+                    case "A":
+                        capView.setText("5.00");
+                        break;
+                    case "A-":
+                        capView.setText("4.50");
+                        break;
+                    case "B+":
+                        capView.setText("4.00");
+                        break;
+                    case "B":
+                        capView.setText("3.50");
+                        break;
+                    case "B-":
+                        capView.setText("3.00");
+                        break;
+                    case "C+":
+                        capView.setText("2.50");
+                        break;
+                    case "C":
+                        capView.setText("2.00");
+                        break;
+                    case "D+":
+                        capView.setText("1.50");
+                        break;
+                    case "D":
+                        capView.setText("1.00");
+                        break;
+                    case "F":
+                        capView.setText("0.00");
+                        break;
+                    case "S":
+                        capView.setText("0.00");
+                        break;
+                    case "U":
+                        capView.setText("0.00");
+                        break;
+                }
+
+            }
+        }
+
+*/
+
+        public double convertMC(String mc) {
+
+            //RETRIEVE
+            Cursor c = db.getAllNames();
+
+            double numMC = 0.00;
+
+            while (c.moveToNext()) {
+                String mc = c.getString(3);
+
+                if (mc == "S") {
+
+                    numMC = 0.00;
+
+                } else if (mc == "U") {
+
+                    numMC = 0.00;
+
+                }
+
+            }
+            return numMC;
+        }
+
+/*
+                switch (mc) {
+                    case "S":
+                        capView.setText("0.00");
+                        break;
+                    case "U":
+                        capView.setText("0.00");
+                        break;
+                }
+
+            }
+
+        }
+
+*/
+
+        public double calculatorNumerator(String grade, String mc) {
+
+            //RETRIEVE
+            Cursor c = db.getAllNames();
+
+            double counter = 0.00;
+
+            while (c.moveToNext()) {
+
+                counter = counter + Double.parseDouble(c.getString(2)) * convertGrade(c.getString(3));
+
+            }
+
+            return counter;
+        }
+
+        public double calculatorDenominator(String grade, String mc) {
+
+            //RETRIEVE
+            Cursor c = db.getAllNames();
+
+            double counter = 0.00;
+
+            while (c.moveToNext()) {
+
+                counter = counter + convertMC(c.getString(2));
+
+            }
+
+            return counter;
+
+        }
+
+        public void calculatorFinal(String grade, String mc) {
+
+            double finalCAP = calculatorNumerator(grade, mc)/calculatorDenominator(grade, mc);
+
+            capView.setText(Double.toString(finalCAP));
+
         }
 
         final ImageView center = (ImageView) findViewById(R.id.center);
